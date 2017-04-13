abb9ad3a21704c0ee833b382f82fc8a7d1402555
Lucas S. Müller <lucas.smuller@hotmail.com>
2017-04-13 20:21:25 -0300

    feat(Batalha): adicionar classe de batalhas ao projeto.

---
+++ cavaleiros.zodiaco/Batalha.java
@@ -0,0 +1,23 @@
+/*
+ * Class de batalhas entre Saints
+ */
+public class Batalha {
+    private Saint saintUm;
+    private Saint saintDois;
+    
+    public Batalha(Saint saintUm, Saint saintDois) {
+        this.saintUm = saintUm;
+        this.saintDois = saintDois;
+    }
+    
+    public void iniciar() {
+        Categoria catSaintUm = saintUm.getArmadura().getCategoria();
+        Categoria catSaintDois = saintDois.getArmadura().getCategoria();
+        if(catSaintUm.getValor() > catSaintDois.getValor())
+            saintDois.perderVida(10);
+        else if(catSaintDois.getValor() > catSaintUm.getValor())
+            saintUm.perderVida(10);
+        else
+            saintDois.perderVida(10);
+    }
+}
