ed67bfdb54a2409a9e5ec0540baa5cdd951c9276
Lucas S. Müller <lucas.muller@cwi.com.br>
2017-04-17 16:08:44 -0300

    test(BatalhaTest): corrigir cases com mais verificações.

--- cavaleiros.zodiaco/BatalhaTest.java
+++ cavaleiros.zodiaco/BatalhaTest.java
@@ -8,29 +8,29 @@ public class BatalhaTest {
     public void aoIniciarSaintDoisDeMenorCategoriaPerdeVida() {
         Saint milo = new Saint("Milo", new Armadura("Escorpião", Categoria.OURO));
         Saint ares = new Saint("Ares", new Armadura("Touro", Categoria.BRONZE));
-        double vidaAres = ares.getVida();
         Batalha epicBattle = new Batalha(milo, ares);
         epicBattle.iniciar();
-        assertEquals((vidaAres - 10), ares.getVida(), 0.01);
+        assertEquals(100, milo.getVida(), 0.01);
+        assertEquals(90, ares.getVida(), 0.01);
     }
     
     @Test
     public void aoIniciarSaintUmDeMenorCategoriaPerdeVida() {
         Saint milo = new Saint("Milo", new Armadura("Escorpião", Categoria.BRONZE));
         Saint ares = new Saint("Ares", new Armadura("Touro", Categoria.OURO));
-        double vidaMilo = milo.getVida();
         Batalha epicBattle = new Batalha(milo, ares);
         epicBattle.iniciar();
-        assertEquals((vidaMilo - 10), milo.getVida(), 0.01);
+        assertEquals(100, ares.getVida(), 0.01);
+        assertEquals(90, milo.getVida(), 0.01);
     }
     
     @Test
     public void aoIniciarSaintDoisDeMesmaCategoriaPerdeVida() {
         Saint milo = new Saint("Milo", new Armadura("Escorpião", Categoria.OURO));
         Saint ares = new Saint("Ares", new Armadura("Touro", Categoria.OURO));
-        double vidaAres = ares.getVida();
         Batalha epicBattle = new Batalha(milo, ares);
         epicBattle.iniciar();
-        assertEquals((vidaAres - 10), ares.getVida(), 0.01);
+        assertEquals(100, milo.getVida(), 0.01);
+        assertEquals(90, ares.getVida(), 0.01);
     }
 }
