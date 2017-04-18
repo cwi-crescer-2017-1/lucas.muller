ceb76c45d525c9fbd15f631f438d9cf250c8f8f1
Lucas S. Müller <lucas.muller@cwi.com.br>
2017-04-18 15:42:51 -0400

    refactor(Constelacao): melhorar metodo adicionar golpe.

--- cavaleiros.zodiaco/Constelacao.java
+++ cavaleiros.zodiaco/Constelacao.java
@@ -1,18 +1,14 @@
 public class Constelacao {
     private String nome;
     private Golpe[] golpes = new Golpe[3];
+	private int ultimoIndexGolpe = 0;
     
     public Constelacao(String nome) {
         this.nome = nome;
     }
     
     public void adicionarGolpe(Golpe golpe) {
-        for(int i = 0; i < this.golpes.length; i++) {
-            if(this.golpes[i] == null) {
-                this.golpes[i] = golpe;
-                break;
-            }
-        }
+		this.golpes[ultimoIndexGolpe++] = golpe;
     }
     
     public Golpe[] getGolpes() {
