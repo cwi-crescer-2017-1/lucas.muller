326c78b5b96c954afad1bf996be9765655c793cf
Lucas S. Müller <lucas.muller@cwi.com.br>
2017-04-18 15:43:14 -0400

    test(ConstelacaoTest): adicionar novos cases.

--- cavaleiros.zodiaco/ConstelacaoTest.java
+++ cavaleiros.zodiaco/ConstelacaoTest.java
@@ -5,25 +5,29 @@ import org.junit.Test;
 
 public class ConstelacaoTest {
     @Test
-    public void novoGolpeNoIndex0() {
+    public void adicionarUmGolpe() {
         Constelacao c = new Constelacao("Escorpião");
         Golpe g = new Golpe("Meteoro de Pégasos", 50);
         c.adicionarGolpe(g);
         assertEquals(g, c.getGolpes()[0]);
+        assertNull(c.getGolpes()[1]);
+        assertNull(c.getGolpes()[2]);
     }
     
     @Test
-    public void novoGolpeNoIndex1() {
+    public void adicionarDoisGolpes() {
         Constelacao c = new Constelacao("Escorpião");
         Golpe g = new Golpe("Meteoro de Pégasos", 50);
         Golpe g2 = new Golpe("Cometa de Pégaso", 10);
         c.adicionarGolpe(g);
         c.adicionarGolpe(g2);
         assertEquals(g2, c.getGolpes()[1]);
+        assertEquals(g, c.getGolpes()[0]);
+        assertNull(c.getGolpes()[2]);
     }
     
     @Test
-    public void novoGolpeNoIndex2() {
+    public void adicionarTresGolpes() {
         Constelacao c = new Constelacao("Escorpião");
         Golpe g = new Golpe("Meteoro de Pégasos", 50);
         Golpe g2 = new Golpe("Cometa de Pégaso", 10);
@@ -32,5 +36,20 @@ public class ConstelacaoTest {
         c.adicionarGolpe(g2);
         c.adicionarGolpe(g3);
         assertEquals(g3, c.getGolpes()[2]);
+        assertEquals(g2, c.getGolpes()[1]);
+        assertEquals(g, c.getGolpes()[0]);
+    }
+    
+    @Test(expected=ArrayIndexOutOfBoundsException.class)
+    public void adicionarQuatroGolpes() {
+        Constelacao c = new Constelacao("Escorpião");
+        Golpe g = new Golpe("Meteoro de Pégasos", 50);
+        Golpe g2 = new Golpe("Cometa de Pégaso", 10);
+        Golpe g3 = new Golpe("Cólera do Dragão", 25);
+        Golpe g4 = new Golpe("Meteoro do Dragão", 5);
+        c.adicionarGolpe(g);
+        c.adicionarGolpe(g2);
+        c.adicionarGolpe(g3);
+        c.adicionarGolpe(g4);
     }
 }
