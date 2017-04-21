a20cffd7762598826b3e951463454a82b67e32c9
Lucas S. Müller <lucas.smuller@hotmail.com>
2017-04-21 16:24:47 -0300

    feat(Movimento): implementar class de movimentos e vestir armadura.

---
+++ cavaleiros.zodiaco/VestirArmadura.java
@@ -0,0 +1,11 @@
+public class VestirArmadura implements Movimento {
+    private Saint saint;
+    
+    public VestirArmadura(Saint saint) {
+        this.saint = saint;
+    }
+
+    public void executar() {
+        this.saint.vestirArmadura();
+    }
+}
