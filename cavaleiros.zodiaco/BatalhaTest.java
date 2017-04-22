7bb823a282da65c9b6baefef25b449eb3a9cf042
Lucas S. Müller <lucas.smuller@hotmail.com>
2017-04-22 13:10:10 -0300

    test(BatalhaTest): adequar testes as alterações.

--- cavaleiros.zodiaco/BatalhaTest.java
+++ cavaleiros.zodiaco/BatalhaTest.java
@@ -5,16 +5,44 @@ import org.junit.Test;
 
 public class BatalhaTest {
     @Test
-    public void aoIniciarSaintDoisDeMenorCategoriaPerdeVida() throws Exception {
+    public void iniciarBatalhaComMovimentos() throws Exception {
+        GoldSaint milo = new GoldSaint("Milo", "Escorpião");
+        BronzeSaint ares = new BronzeSaint("Ares", "Touro");
+        Golpe basico = new Golpe("Ataque básico", 10);
+        milo.aprenderGolpe(basico);
+        milo.adicionarMovimento(new VestirArmadura(milo));
+        milo.adicionarMovimento(new Golpear(milo, ares));
+        Golpe forte = new Golpe("Ataque básico", 50);
+        ares.aprenderGolpe(forte);
+        ares.adicionarMovimento(new Golpear(ares, milo));
+        Batalha epicBattle = new Batalha(milo, ares);
+        epicBattle.iniciar();
+        assertEquals(0, milo.getVida(), 0.01);
+        assertEquals(60, ares.getVida(), 0.01);
+    }
+    
+    @Test
+    public void iniciarBatalhaSemMovimentos() throws Exception {
         GoldSaint milo = new GoldSaint("Milo", "Escorpião");
         BronzeSaint ares = new BronzeSaint("Ares", "Touro");
         Batalha epicBattle = new Batalha(milo, ares);
         epicBattle.iniciar();
         assertEquals(100, milo.getVida(), 0.01);
-        assertEquals(90, ares.getVida(), 0.01);
+        assertEquals(100, ares.getVida(), 0.01);
     }
     
-    @Test
+    /*@Test
+    public void iniciarBatalhaSemGolpear() throws Exception {
+        GoldSaint milo = new GoldSaint("Milo", "Escorpião");
+        milo.adicionarMovimento(new VestirArmadura(milo));
+        BronzeSaint ares = new BronzeSaint("Ares", "Touro");
+        Batalha epicBattle = new Batalha(milo, ares);
+        epicBattle.iniciar();
+        assertEquals(100, milo.getVida(), 0.01);
+        assertEquals(100, ares.getVida(), 0.01);
+    }*/
+    
+    /*@Test
     public void aoIniciarSaintUmDeMenorCategoriaPerdeVida() throws Exception {
         BronzeSaint milo = new BronzeSaint("Milo", "Escorpião");
         GoldSaint ares = new GoldSaint("Ares", "Touro");
@@ -32,5 +60,5 @@ public class BatalhaTest {
         epicBattle.iniciar();
         assertEquals(100, milo.getVida(), 0.01);
         assertEquals(90, ares.getVida(), 0.01);
-    }
+    }*/
 }
