7ecac6eee870d0fb73198d35f970b5f3e08d3997
Lucas S. Müller <lucas.smuller@hotmail.com>
2017-04-24 12:01:01 -0300

    feat(Batalha): adicionar comentários para melhor entendimento.

--- cavaleiros.zodiaco/Batalha.java
+++ cavaleiros.zodiaco/Batalha.java
@@ -12,12 +12,17 @@ public class Batalha {
     
     public void iniciar() {
         Saint golpeadorAtual = saintUm;
+        // verifica se o saintDois tem categoria maior que um
         if(saintDois.getArmadura().getCategoria().getValor() > saintUm.getArmadura().getCategoria().getValor())
             golpeadorAtual = saintDois;
         
+        // verifica se os saints tem movimentos de dano e golpes para evitar loop infinito
         if(
             (saintUm.hasMovimentos() || saintDois.hasMovimentos()) 
-            && (saintUm.hasMovimentosDeDano() || saintDois.hasMovimentosDeDano() && (saintUm.hasGolpes() || saintDois.hasGolpes()))   
+            && (
+                (saintUm.hasMovimentosDeDano() || saintDois.hasMovimentosDeDano()) 
+                && (saintUm.hasGolpes() || saintDois.hasGolpes())
+            )   
         ) {
             do {
                 Movimento mov = golpeadorAtual.getProximoMovimento();
