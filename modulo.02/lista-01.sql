25e9a1c097e636f804573ebfe3922458d8e521e9
Lucas S. Müller <lucas.muller@cwi.com.br>
2017-04-28 15:39:37 -0300

    doc(lista-01): alterações em queries.

--- modulo.02/lista-01.sql
+++ modulo.02/lista-01.sql
@@ -14,18 +14,18 @@
 
 -- exercício 2:
 -- truncate table CidadeAux   ou   delete from CidadeAux
--- insert into CidadeAux select * from Cidade where UF='RS'
+-- insert into CidadeAux (IDCidade, Nome, UF) select IDCidade, Nome, UF from Cidade where UF='RS'
 
 -- exercício 3:
 /*
 create table Produtos (
 	IDProduto       int identity not null,
-	Nome            varchar(50) not null,
+	Nome            varchar(50)  not null,
 	NomeDescritivo  varchar(250) not null,
-	DataCriacao     datetime not null,
+	DataCriacao     date         not null,
 	LocalEstoque    varchar(100) not null,
-	Quantidade      int not null,
-	Preco           real not null,
+	Quantidade      decimal(7,3) not null,
+	Preco           decimal(9,2) not null,
 		constraint PK_IDProduto primary key(IDProduto)
 )
 */
@@ -36,3 +36,6 @@ insert into Produtos (Nome, NomeDescritivo, DataCriacao, LocalEstoque, Quantidad
 	('TV Samsung 42"', 'SmartTV 42 polegadas da Samsung com conexão a Internet', GETDATE(), 'Corredor 5B', 10, 1499.99),
 	('Computador Gamer', 'Computador Gamer com Intel i7-7700k, 16GB RAM, HD de 1TB e GTX 1080TI', GETDATE(), 'Corredor 7C', 5, 7999.99)
 */
+
+-- converter string para datetime/date: select convert(datetime, '28/04/2017', 103) as FormatoDateTime
+-- formato 103 = dd/mm/aaaa             select convert(date, '28/04/2017', 103)     as FormatoDate
