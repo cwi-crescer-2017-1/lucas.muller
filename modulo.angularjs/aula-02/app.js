2db8b1058cec8d74c4165374b26657e22871a9eb
Lucas S. Müller <lucas.muller@cwi.com.br>
2017-05-18 18:37:37 -0300

    doc(aula-02): exercício 2 e 3 da aula 02.

--- modulo.angularjs/aula-02/app.js
+++ modulo.angularjs/aula-02/app.js
@@ -1,9 +1,103 @@
 var app = angular.module('app', []);
 
+app.run(function() {
+    console.log('App carregado com sucesso!');
+});
+
 app.controller('Ex01Ctrl', ['$scope', '$filter', function($s, $filter) {
     $s.converterData = function(data) {
-        data = data.split('/');
-        data = `${data[1]}/${data[0]}/${data[2]}`;
+        data = data.replace(/([0-9]{2})[- /.]([0-9]{2})[- /.]([0-9]{4})/ig, '$2/$1/$3');
         $s.dataFormatada = $filter('date')(new Date(data), 'mediumDate');
     };
 }]);
+
+app.controller('Ex02Ctrl', ['$scope', function($s){
+    $s.instrutores = [{
+        nome: 'Bernardo',
+        aula: [{
+            numero: 1,
+            nome: 'OO'
+        },
+        {
+            numero: 4,
+            nome: 'Javascript'
+        }
+        ]
+    },
+    {
+        nome: 'Nunes',
+        aula: [{
+        numero: 2,
+        nome: 'Banco de Dados I'
+        }]
+    },
+    {
+        nome: 'Pedro (PHP)',
+        aula: [{
+        numero: 3,
+        nome: 'HTML e CSS'
+        }]
+    },
+    {
+        nome: 'Zanatta',
+        aula: [{
+        numero: 5,
+        nome: 'AngularJS'
+        }]
+    }
+    ];
+}]);
+
+app.controller('Ex03Ctrl', ['$scope', function($s){
+    $s.instrutores = [{
+        nome: 'Bernardo',
+        aula: [{
+            numero: 1,
+            nome: 'OO'
+        },
+        {
+            numero: 4,
+            nome: 'Javascript'
+        }
+        ]
+    },
+    {
+        nome: 'Nunes',
+        aula: [{
+        numero: 2,
+        nome: 'Banco de Dados I'
+        }]
+    },
+    {
+        nome: 'Pedro (PHP)',
+        aula: [{
+        numero: 3,
+        nome: 'HTML e CSS'
+        }]
+    },
+    {
+        nome: 'Zanatta',
+        aula: [{
+        numero: 5,
+        nome: 'AngularJS'
+        }]
+    }
+    ];
+    $s.arrayAulas = transformarArrayInstrutores($s.instrutores);
+
+    function transformarArrayInstrutores(array) {
+        var novoArray = [];
+        angular.forEach(array, function(instrutor) {
+            angular.forEach(instrutor.aula, function(aula) {
+                novoArray.push({numero: aula.numero, nome: aula.nome, instrutor: instrutor.nome});
+            });
+        });
+        return novoArray;
+    };
+}]);
+
+app.filter('mascada', function() {
+    return function(valor) {
+        return valor.replace(/(nunes)/ig, '$ $1 $');
+    };
+});
