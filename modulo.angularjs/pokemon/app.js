37d15e920387444a92ef3631e10fb5b9e400db5e
Lucas S. Müller <lucas.muller@cwi.com.br>
2017-05-19 15:37:25 -0300

    doc(pokemon): algumas melhorias.

--- modulo.angularjs/pokemon/app.js
+++ modulo.angularjs/pokemon/app.js
@@ -26,20 +26,3 @@ app.filter('capitalize', function() {
       return (!!input) ? input.charAt(0).toUpperCase() + input.substr(1).toLowerCase() : '';
     }
 });
-
-app.service('funcoes', function() {
-    this.hexToRgb = function(hex) {
-        // script retirado de http://stackoverflow.com/questions/5623838/rgb-to-hex-and-hex-to-rgb
-        var shorthandRegex = /^#?([a-f\d])([a-f\d])([a-f\d])$/i;
-        hex = hex.replace(shorthandRegex, function(m, r, g, b) {
-            return r + r + g + g + b + b;
-        });
-
-        var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
-        return result ? {
-            r: parseInt(result[1], 16),
-            g: parseInt(result[2], 16),
-            b: parseInt(result[3], 16)
-        } : null;
-    }
-});
