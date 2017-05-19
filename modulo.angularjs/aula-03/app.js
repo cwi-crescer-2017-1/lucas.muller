eb96ea22796016a90d511c1919c53edc4d2e7a87
Lucas S. Müller <lucas.muller@cwi.com.br>
2017-05-19 18:30:32 -0300

    doc(aula-03): scripts da terceira aula de AngularJS.

---
+++ modulo.angularjs/aula-03/app.js
@@ -0,0 +1,48 @@
+var app = angular.module('app', []);
+
+app.controller('InstrutoresCtrl', function($scope) {
+    $scope.instrutores = [
+        {
+            nome: 'Bernardo',
+            sobrenome: 'Rezende',
+            idade: 30,
+            email: 'bernardo@cwi.com.br',
+            jaDeuAula: true,
+            aula: 'OO'
+        },
+        {
+            nome: 'André',
+            sobrenome: 'Nunes',
+            idade: 35,
+            email: 'andre.nunes@cwi.com.br',
+            jaDeuAula: true,
+            aula: 'Banco de Dados I'
+        }
+    ];
+
+    $scope.aulas = [
+        'OO',
+        'HTML e CSS',
+        'Javascript',
+        'AngularJS',
+        'Banco de Dados I'
+    ];
+
+    $scope.adicionarInstrutor = function(instrutor) {
+        $scope.instrutores.push(angular.copy(instrutor));
+        $scope.novoInstrutor = {};
+        $scope.formAddInstrutor.$setPristine();
+    };
+
+    $scope.removerInstrutor = function(index) {
+        $scope.instrutores.splice(index, 1);
+        $scope.removerInstrutor = {};
+        $scope.formExcInstrutor.$setPristine();
+    };
+});
+
+app.filter('booleanToYesOrNo', function() {
+    return function(boolean) {
+        return boolean?"Sim":"Não";
+    };
+});
