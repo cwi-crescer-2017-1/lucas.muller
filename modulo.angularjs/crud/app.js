ded1ac9845d8f97326394a6b9d8aa715a7d2ee21
Lucas S. Müller <lucas.muller@cwi.com.br>
2017-05-20 00:33:32 -0300

    feat(crud): adicionar form p/ adicionar e remover aulas.

--- modulo.angularjs/crud/app.js
+++ modulo.angularjs/crud/app.js
@@ -9,7 +9,7 @@ app.controller('CrudCtrl', function($scope) {
             idade: 30,
             email: 'bernardo@cwi.com.br',
             jaDeuAula: true,
-            aula: [0, 2]
+            aula: ["0", "2"]
         },
         {
             id: 1,
@@ -18,7 +18,7 @@ app.controller('CrudCtrl', function($scope) {
             idade: 35,
             email: 'andre.nunes@cwi.com.br',
             jaDeuAula: true,
-            aula: [4]
+            aula: ["4"]
         }
     ];
 
@@ -36,19 +36,47 @@ app.controller('CrudCtrl', function($scope) {
         $scope.instrutores.push(nInstrutor);
         $scope.novoInstrutor = {};
         $scope.formAddInstrutor.$setPristine();
+        alert("Instrutor adicionado com sucesso!");
     };
 
-    $scope.removerInstrutor = function(index) {
+    $scope.removerInstrutor = function(id) {
+        var index = $scope.instrutores.findIndex(e => e.id == id);
+        if(index === -1) return;
         $scope.instrutores.splice(index, 1);
-        $scope.removerInstrutor = {};
+        $scope.removerInstrutor.id = "";
         $scope.formExcInstrutor.$setPristine();
+        alert("Instrutor removido com sucesso!");
+    };
+
+    $scope.adicionarAula = function(aula) {
+        var nAula = angular.copy(aula);
+        nAula.id = $scope.aulas.length;
+        $scope.aulas.push(nAula);
+        $scope.novaAula = {};
+        $scope.formAddAula.$setPristine();
+        alert("Aula adicionada com sucesso!");
+    };
+    
+    $scope.removerAula = function(id) {
+        var aulaTemVinculos = $scope.instrutores.some(e=>e.aula.includes(""+id));
+        if(aulaTemVinculos) {
+            alert("Impossível remover aula pois há instrutores vinculados à ela!");
+            return;
+        } else {
+            var index = $scope.aulas.findIndex(e => e.id == id);
+            if(index === -1) return;
+            $scope.aulas.splice(index, 1);
+            $scope.removerAula.id = "";
+            $scope.formExcAula.$setPristine();
+            alert("Aula removida com sucesso!");
+        }
     };
 
     $scope.getAulas = function(ids) {
         var arr = [];
-        ids.forEach(e => arr.push($scope.aulas[e]));
+        ids.forEach(e => arr.push($scope.aulas.find(a => a.id == e)));
         return arr;
-    }
+    };
 });
 
 app.filter('booleanToYesOrNo', function() {
