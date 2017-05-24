ec8e7394f32dfea1a7ffb4c210b764b0ee45f50c
Lucas S. Müller <lucas.muller@cwi.com.br>
2017-05-24 17:08:08 -0300

    feat(aula1-dotnet): adicionar exerc. da aula 01.

---
+++ modulo.dotnet/projetos/Aula1/Demo1/IMC.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo1
+{
+    public class IMC
+    {
+        public double Peso { get; set; }
+        public double Altura { get; set; }
+
+        public IMC(double peso, double altura)
+        {
+            this.Peso = peso;
+            this.Altura = altura;
+        }
+
+        public double CalculaIMC()
+        {
+            return this.Peso / Math.Pow(this.Altura, 2);
+        }
+    }
+}
