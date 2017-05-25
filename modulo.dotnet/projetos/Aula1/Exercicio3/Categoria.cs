5521dea0fea844de94dc338761a71bb69723941a
Lucas S. Müller <lucas.muller@cwi.com.br>
2017-05-24 22:28:31 -0300

    fix(gitignore): re-adicionar coisas úteis.

---
+++ modulo.dotnet/projetos/Aula1/Exercicio3/Categoria.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio3
+{
+    public enum Categoria
+    {
+        Comercio = 170,
+        Industria = 200
+    }
+}
---
+++ modulo.dotnet/projetos/LucasMuller/Exercicio3/IFolhaPagamento.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio3
+{
+    public interface IFolhaPagamento
+    {
+        Demonstrativo GerarDemonstrativo(int horasCategoria, double salarioBase, double horasExtras, double horasDescontadas);
+    }
+}
