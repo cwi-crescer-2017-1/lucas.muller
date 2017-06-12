7671c88ae5e6a259f087ff6721ac7a3a6767d672
Lucas S. Müller <lucas.muller@cwi.com.br>
2017-06-12 17:23:53 -0300

    doc(scripts-lista01): implementar queries da lista 01 do modulo 07.

---
+++ modulo.oracle/Scripts da lista de exerc. 01.sql
@@ -0,0 +1,33 @@
+/* -- LISTA DE EXERCÍCIO 01 -- */
+
+/* 01 - Liste os produtos (id e nome) que não tiveram nenhuma compra nos últimos quatro meses. */
+SELECT * FROM PRODUTO
+WHERE IDPRODUTO NOT IN (
+  SELECT ITEM.IDPRODUTO
+  FROM PEDIDOITEM ITEM
+  JOIN PEDIDO PED ON PED.IDPEDIDO = ITEM.IDPEDIDO
+  WHERE PED.DATAPEDIDO >= ADD_MONTHS(SYSDATE,-4)
+  GROUP BY ITEM.IDPRODUTO
+)
+ORDER BY IDPRODUTO;
+
+/* 02 - Altere o status dos produtos (campo situacao) que não tiveram nenhum pedido nos últimos quatro meses. */
+UPDATE PRODUTO
+SET SITUACAO = 'L'
+WHERE IDPRODUTO NOT IN (
+  SELECT ITEM.IDPRODUTO
+  FROM PEDIDOITEM ITEM
+  JOIN PEDIDO PED ON PED.IDPEDIDO = ITEM.IDPEDIDO
+  WHERE PED.DATAPEDIDO >= ADD_MONTHS(SYSDATE,-4)
+  GROUP BY ITEM.IDPRODUTO
+);
+COMMIT; ROLLBACK;
+
+/* 03 - Faça uma consulta que receba um parâmetro sendo o IDProduto e liste a quantidade de itens 
+        na tabela PedidoItem com este IDProduto foram vendidos no último ano (desde janeiro/2017). */
+SELECT ITEM.IDPRODUTO, COUNT(*) AS QUANTIDADE
+FROM PEDIDOITEM ITEM
+JOIN PEDIDO PED ON PED.IDPEDIDO = ITEM.IDPEDIDO
+WHERE PED.DATAPEDIDO >= TO_DATE('2017-01-01', 'yyyy-mm-dd')
+  AND ITEM.IDPRODUTO = :IDPRODUTO
+GROUP BY ITEM.IDPRODUTO;
