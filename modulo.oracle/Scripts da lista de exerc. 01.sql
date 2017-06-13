7da74b5a78b4692dfee198ff1b91459009513260
Lucas S. Müller <lucas.muller@cwi.com.br>
2017-06-13 14:56:02 -0300

    fix(scripts-lista01): adicionar comentarios e corrigir queries.

--- modulo.oracle/Scripts da lista de exerc. 01.sql
+++ modulo.oracle/Scripts da lista de exerc. 01.sql
@@ -1,8 +1,17 @@
-/* -- LISTA DE EXERCÍCIO 01 -- */
+/* ---- LISTA DE EXERCÍCIO 01 ---- */
+/*      https://goo.gl/UAkKRw      */
+
+/* Atualizar estatísticas manualmente p/ melhorar desempenho: */
+EXEC DBMS_STATS.GATHER_SCHEMA_STATS(USER); 
+SELECT TABLE_NAME, TABLESPACE_NAME, NUM_ROWS, BLOCKS, LAST_ANALYZED FROM USER_TABLES;
 
 /* 01 - Liste os produtos (id e nome) que não tiveram nenhuma compra nos últimos quatro meses. */
+CREATE OR REPLACE VIEW vwProdutos_Sem_Compras AS
 SELECT IDPRODUTO, NOME FROM PRODUTO
 WHERE IDPRODUTO NOT IN (
+  /* Poderia ter sido criada uma view para este select:
+  
+  CREATE OR REPLACE VIEW vwProdutosRecentes AS */
   SELECT ITEM.IDPRODUTO
   FROM PEDIDOITEM ITEM
   JOIN PEDIDO PED ON PED.IDPEDIDO = ITEM.IDPEDIDO
@@ -13,21 +22,13 @@ ORDER BY IDPRODUTO;
 
 /* 02 - Altere o status dos produtos (campo situacao) que não tiveram nenhum pedido nos últimos quatro meses. */
 UPDATE PRODUTO
-SET SITUACAO = 'L'
-WHERE IDPRODUTO NOT IN (
-  SELECT ITEM.IDPRODUTO
-  FROM PEDIDOITEM ITEM
-  JOIN PEDIDO PED ON PED.IDPEDIDO = ITEM.IDPEDIDO
-  WHERE PED.DATAPEDIDO >= ADD_MONTHS(TRUNC(SYSDATE),-4)
-  GROUP BY ITEM.IDPRODUTO
-);
-COMMIT; ROLLBACK;
+SET SITUACAO = 'I'
+WHERE IDPRODUTO IN (SELECT IDPRODUTO FROM VWPRODUTOS_SEM_COMPRAS);
 
 /* 03 - Faça uma consulta que receba um parâmetro sendo o IDProduto e liste a quantidade de itens 
         na tabela PedidoItem com este IDProduto foram vendidos no último ano (desde janeiro/2017). */
-SELECT ITEM.IDPRODUTO, COUNT(*) AS QUANTIDADE
+SELECT SUM(ITEM.QUANTIDADE) AS QUANTIDADE
 FROM PEDIDOITEM ITEM
 JOIN PEDIDO PED ON PED.IDPEDIDO = ITEM.IDPEDIDO
-WHERE PED.DATAPEDIDO >= TO_DATE('2017-01-01', 'yyyy-mm-dd')
-  AND ITEM.IDPRODUTO = :IDPRODUTO
-GROUP BY ITEM.IDPRODUTO;
+WHERE PED.DATAPEDIDO >= TRUNC(SYSDATE, 'YYYY')
+  AND ITEM.IDPRODUTO = :IDPRODUTO;
